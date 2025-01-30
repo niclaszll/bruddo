d2d2baed613414a36c10310f0b651cf04441cfe5
niclaszll <22919812+niclaszll@users.noreply.github.com>
2025-01-30 08:47:49 +0100

    fix: header z-index

--- src/app/(home)/client.tsx
+++ src/app/(home)/client.tsx
@@ -39,7 +39,7 @@ export default function HomeClient() {
         <SidebarRail />
       </Sidebar>
       <SidebarInset>
-        <header className="sticky top-0 flex h-16 shrink-0 items-center justify-between gap-2 border-b bg-background px-4">
+        <header className="sticky top-0 flex h-16 shrink-0 items-center justify-between gap-2 border-b bg-background px-4 z-10">
           <div className="flex items-center gap-2">
             <SidebarTrigger className="-ml-1" />
             <Separator
