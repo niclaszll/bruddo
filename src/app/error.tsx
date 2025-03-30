4dba44eaf215c0494f1deead8c40a7c1e166320b
niclaszll <22919812+niclaszll@users.noreply.github.com>
2025-03-30 15:35:04 +0200

    feat: add loading and error component

---
+++ src/app/error.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { Spinner } from '@/components/ui/loading-spinner';
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Spinner />
+    </div>
+  );
+}
